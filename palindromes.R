303121f43c653fbb8078649d352d11420a476ebf
costlysignalling <39238948+costlysignalling@users.noreply.github.com>
2020-04-20 12:51:10 +0200

    Update palindromes.R

--- palindromes.R
+++ palindromes.R
@@ -1,13 +1,15 @@
 #Load the data as a vector
-d<-read.table("words_alpha.txt")
+d<-read.table("words_czech_ANSI.txt") #for czech
+#d<-read.table("words_alpha.txt")     #for english
+
 v<-as.character(d[,1])
+
+length(v)
 
 palindrome<-function(string){
   splitted<-strsplit(string,"")
   paste(rev(splitted[[1]]),collapse="")
 }
-
-palindrome("prd")
 
 pal<-c(sapply(v,palindrome))
 
@@ -22,5 +24,8 @@ d$palindromatic<-ifelse(d$first==d$second,T,F)
 nrow(d)
 sum(d$palindromatic)
 
-write.table(d,"palindromatic_english.txt",sep="\t",row.names = F)
+write.table(d,"palindromatic_czech.txt",sep="\t",row.names = F)
+#write.table(d,"palindromatic_english.txt",sep="\t",row.names = F)
+
+
 
